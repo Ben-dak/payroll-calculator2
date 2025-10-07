630e0c33a779d33ff50fe7c51853ddcb3b052b5d
Dominick <dominickfrench.oe@gmail.com>
2025-10-07 11:16:18 -0500

    Fourth commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
         String fileName = "employees.csv";
+        String input;
 
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            while((input = bufReader.readLine()) != null) {
+                System.out.println(input);
+            }
+
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
