92111a5adf65033b47d570ceb7c51eac8c5d327b
Dominick <dominickfrench.oe@gmail.com>
2025-10-07 09:59:50 -0500

    Second commit

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,7 +1,22 @@
 public class Employee {
-    int eeId;
-    String name;
-    double hoursWorked;
-    double payRate;
+    private int eeId;
+    private String eeName;
+    private double hoursWorked;
+    private double payRate;
 
+    public Employee(int id, String name, double hoursWorked, double payRate) {
+        this.eeId = id;
+        this.eeName = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getId() { return eeId; }
+    public String getName() { return eeName; }
+    public double getHoursWorked() { return hoursWorked; }
+    public double getPayRate() { return payRate; }
+
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
 }
